642d003b51c818c5b7906e5f25a9969940f4ada0
PC <prithachanana@gmail.com>
2018-01-16 22:37:44 -0600

    Calculate protein mass

---
+++ prtm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+##Calculate protein mass from monoisotopic mass table
+
+import argparse
+
+def main(protein,mass):
+    return "%.3f" % sum([mass[x] for x in protein])
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-i",metavar="Input file",required=True)
+    args=parser.parse_args()
+    monoisotope={'A':71.03711,'C':103.00919,'D':115.02694,'E':129.04259,'F':147.06841,'G':57.02146,'H':137.05891,'I':113.08406,'K':128.09496,'L':113.08406,'M':131.04049,'N':114.04293,'P':97.05276,'Q':128.05858,'R':156.10111,'S':87.03203,'T':101.04768,'V':99.06841,'W':186.07931,'Y':163.06333}
+    with open(args.i,"r") as f:
+        print main(f.readline().rstrip(),monoisotope)
